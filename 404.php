7bf376440c48f061b07fdc77bc5b5ed4d9450833
loftor <i@loftor.com>
2014-01-13 23:42:02 +0800

    hah

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php $this->need('header.php'); ?>
 
-    <div class="col-mb-12 col-tb-8 col-tb-offset-2">
+    <div class="col-mb-12 col-tb-9 col-tb-offset-2">
 
         <div class="error-page">
             <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
